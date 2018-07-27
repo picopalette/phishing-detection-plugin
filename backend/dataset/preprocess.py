4731802f47ff9e2840ddfc3b273701cb79570944
ramkumar <ramukmar1998@gmail.com>
2018-07-27 16:37:30 +0530

    Train classifier and accuracy score

--- backend/dataset/preprocess.py
+++ backend/dataset/preprocess.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[10]:
+# In[1]:
 
 
 import arff
@@ -9,24 +9,25 @@ import numpy as np
 from sklearn.model_selection import train_test_split, KFold
 
 
-# In[3]:
+# In[2]:
 
 
 dataset = arff.load(open('dataset.arff', 'r'))
 data = np.array(dataset['data'])
 
 
-# In[4]:
+# In[3]:
 
 
 print('The dataset has {0} datapoints with {1} features'.format(data.shape[0], data.shape[1]-1))
 print('Features: {0}'.format([feature[0] for feature in dataset['attributes']]))
 
 
-# In[8]:
+# In[6]:
 
 
-X, y = data[:, :-1], data[:, [-1]]
+X, y = data[:, :-1], data[:, -1]
+y.reshape(y.shape[0])
 print('Before spliting')
 print('X:{0}, y:{1}'.format(X.shape, y.shape))
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
