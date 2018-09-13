f816601c92d8df5e1e32af4e74551923dd8716b8
caped-speedster <aswinsundar17@gmail.com>
2018-09-13 09:20:05 +0530

    14 features extraction

--- frontend/features.js
+++ frontend/features.js
@@ -79,10 +79,11 @@ if(patt.test(urlDomain)){
 
 //---------------------- 7.  No. of Sub Domains  ----------------------
 
-patt=/./;
-if((onlyDomain.match(patt)||[]).length=1){ 
+//patt=".";
+
+if((onlyDomain.match(RegExp('\\.','g'))||[]).length==1){ 
     result["No. of Sub Domains"]="-1";
-}else if((onlyDomain.match(patt)||[]).length=2){ 
+}else if((onlyDomain.match(RegExp('\\.','g'))||[]).length==2){ 
     result["No. of Sub Domains"]="0";    
 }else{
     result["No. of Sub Domains"]="1";
@@ -102,34 +103,159 @@ if(patt.test(url)){
 
 //---------------------- 10. Favicon  ----------------------
 
-/*
-alert("hello there");
-var msg ="..";
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        alert("working");
-      //msg+=chrome.webRequest.onBeforeRequest.frameAncestors[0];
-    },
-    {urls: ["<all_urls>"]},
-    []);
-alert(msg);
-*/
+var favicon = undefined;
+var nodeList = document.getElementsByTagName("link");
+for (var i = 0; i < nodeList.length; i++)
+{
+    if((nodeList[i].getAttribute("rel") == "icon")||(nodeList[i].getAttribute("rel") == "shortcut icon"))
+    {
+        favicon = nodeList[i].getAttribute("href");
+    }
+}
+if(favicon.length==12){
+    result["Favicon"]="-1";
+}else{
+    patt=RegExp(urlDomain,'g');
+    if(patt.test(favicon)){
+        result["Favicon"]="-1";
+    }else{
+        result["Favicon"]="1";
+    }
+}
+
 
 //---------------------- 11. Using Non-Standard Port  ----------------------
+
+result["Port"]="-1";
 
 //---------------------- 12.  HTTPS in URL's domain part  ----------------------
 
 
 patt=/https/;
 if(patt.test(onlyDomain)){
-    result["HTTPS in URL's domain part"]=",1";
+    result["HTTPS in URL's domain part"]="1";
 }else{
-    result["HTTPS in URL's domain part"]=",-1";
+    result["HTTPS in URL's domain part"]="-1";
 }
 
 // alert(result);
 
+//---------------------- 13.  Request URL  ----------------------
+
+var imgTags = document.getElementsByTagName("img");
+
+var phishCount=0;
+var legitCount=0;
+
+patt=RegExp(onlyDomain,'g');
+
+for(var i = 0; i < imgTags.length; i++){
+    var src = imgTags[i].getAttribute("src");
+    if(patt.test(src)){
+        legitCount++;
+    }else if(src.charAt(0)=='/'&&src.charAt(1)!='/'){
+        legitCount++;
+    }else{
+        phishCount++;
+    }
+}
+var totalCount=phishCount+legitCount;
+var outRequest=(phishCount/totalCount)*100;
+//alert(outRequest);
+
+if(outRequest<22){
+    result["Request URL"]="-1";
+}else if(outRequest>=22&&outRequest<61){
+    result["Request URL"]="0";
+}else{
+    result["Request URL"]="1";
+}
+
+//---------------------- 14.  URL of Anchor  ----------------------
+var aTags = document.getElementsByTagName("a");
+
+phishCount=0;
+legitCount=0;
+var allhrefs="";
+
+for(var i = 0; i < aTags.length; i++){
+    var hrefs = aTags[i].getAttribute("href");
+    allhrefs+=hrefs+"       ";
+    if(patt.test(hrefs)){
+        legitCount++;
+    }else if(hrefs.charAt(0)=='#'||(hrefs.charAt(0)=='/'&&hrefs.charAt(1)!='/')){
+        legitCount++;
+    }else{
+        phishCount++;
+    }
+}
+totalCount=phishCount+legitCount;
+outRequest=(phishCount/totalCount)*100;
+
+if(outRequest<31){
+    result["Anchor"]="-1";
+}else if(outRequest>=31&&outRequest<=67){
+    result["Anchor"]="0";
+}else{
+    result["Anchor"]="1";
+}
+
+//alert(allhrefs);
+
+//---------------------- 15. Links in script and link  ----------------------
+
+var mTags = document.getElementsByTagName("meta");
+var sTags = document.getElementsByTagName("script");
+var lTags = document.getElementsByTagName("link");
+
+phishCount=0;
+legitCount=0;
+
+allhrefs="sTags  ";
+
+for(var i = 0; i < sTags.length; i++){
+    var sTag = sTags[i].getAttribute("src");
+    if(sTag!=null){
+        allhrefs+=sTag+"      ";
+        if(patt.test(sTag)){
+            legitCount++;
+        }else if(sTag.charAt(0)=='/'&&sTag.charAt(1)!='/'){
+            legitCount++;
+        }else{
+            phishCount++;
+        }
+    }
+}
+
+allhrefs+="      lTags   ";
+for(var i = 0; i < lTags.length; i++){
+    var lTag = lTags[i].getAttribute("href");
+    allhrefs+=lTag+"       ";
+    if(patt.test(lTag)){
+        legitCount++;
+    }else if(lTag.charAt(0)=='/'&&lTag.charAt(1)!='/'){
+        legitCount++;
+    }else{
+        phishCount++;
+    }
+}
+
+totalCount=phishCount+legitCount;
+outRequest=(phishCount/totalCount)*100;
+
+if(outRequest<17){
+    result["Script & Link"]="-1";
+}else if(outRequest>=17&&outRequest<=81){
+    result["Script & Link"]="0";
+}else{
+    result["Script & Link"]="1";
+}
+
+//alert(allhrefs);
+
+//---------------------- Sending the result  ----------------------
+
 chrome.runtime.sendMessage(result, function(response) {
-    console.log(result);
-    console.log(response);
+    //console.log(result);
+    //console.log(response);
 });
